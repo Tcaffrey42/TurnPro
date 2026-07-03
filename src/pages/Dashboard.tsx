be7562a7eb166f0b7c1a116864b832dbd8b8c993
Tcaffrey42 <tcaffrey42@gmail.com>
2026-07-03 12:53:42 -0400

    Delete src directory

--- src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard(){
-return <div className="container">
-<h1>Turn<span style={{color:"red"}}>Pro</span></h1>
-<div className="card">
-<h2>Executive Dashboard</h2>
-<p>Active Turns: 126</p>
-</div>
-</div>;
-}
